74b1fde871a9f0ccca5508126af59fb5c4739cd0
Ramcharan <sherlaramcharan25@gmail.com>
2026-05-17 21:40:52 +0530

    Time: 20 ms (8.82%) | Memory: 48.1 MB (5.50%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String str=s.toLowerCase();
+        // boolean b=true;
+        String regex = "[^a-zA-Z0-9\\s]";
+        str=str.replaceAll(regex,"");
+        str=str.replaceAll(" ","");
+        for(int i=0;i<str.length()/2;i++){
+            if(str.charAt(i)==str.charAt(str.length()-1-i)){
+                continue;
+
+            }else{
+                // b=false;
+                return false;
+                // break
+            }
+        }
+        // if(b){
+            return true;
+        // }else{
+        //     return false;
+        // }
+
+
+        
+    }
+}
